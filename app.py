df28968d71b847f6f43351292925ce46ee726d1d
t3qquq <t3qquq@gmail.com>
2023-01-16 18:06:03 +0900

    [feat] db에서 인용할 영화 대사 정보 가져오기 #2

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
+
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.jq6qj7m.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0", port="5000", debug=True)
+
+@app.route('/line', methods=['GET'])
+def line_get():
+    movie_lines = list(db.movieLines.find({},{'_id': False}))
+    line_receive = request.args.get('line_give')
+    return jsonify({'movieLines': movie_lines})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
