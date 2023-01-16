f8fe13edab8a01644c6b3c7d2fd718351e8553ad
Ryum <ryumforart@gmail.com>
2023-01-17 05:28:28 +0900

    fix : fix bugs

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,42 @@
+// 영화 대사 관련 정보 중 하나의 대사를 랜덤으로 리턴
+// response['movieLines'][random_int()] 리턴 형태:
+//
+//         {
+//             "movie": "Star Wars: Episode V - The Empire Strikes Back",
+//             "num": 0,
+//             "quote": "Do, or do not. There is no try.",
+//             "type": "movie",
+//             "year": 1890
+//         }
+/*
+function extract_random_line() {
+    $.ajax({
+        type: 'GET',
+        url: '/line',
+        data: {},
+        success: function (response) {
+            {
+                return response['movieLines'][random_int()]['quote'];
+            }
+        }
+    })
+}
+*/
+//upperSide = Protocol
+
+//lowerSide = Main contents implement
 
 const quoteDisplayElement = document.getElementById('scriptContainer');
 const quoteInputElement = document.getElementById('text-input');
 let wordSelectorIndex = 0;
+
+//wpm & acc var
+const wpmTag = document.getElementById('wpm-value');
+const accTag = document.getElementById('acc-value');
+let solveCount = 0;
+let wholeCount = 0;
+let failureCount = 0;
+
 
 // Test quote generater.
 const quote = 'This is the test quote. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis tortor placerat, semper libero non, tincidunt enim. Pellentesque hendrerit porta est, ut placerat purus. Etiam pulvinar neque sem, nec imperdiet orci aliquet vel. Fusce ut gravida felis, nec blandit diam. Donec fermentum luctus metus vel tempor. Vivamus pharetra, libero at rutrum commodo, ipsum turpis malesuada sapien, a blandit justo sem vitae lacus. Donec fringilla maximus tellus, a eleifend quam cursus at. Donec maximus ornare eros, nec eleifend tellus. Mauris nec pretium urna. Suspendisse tincidunt accumsan velit. Integer nec eleifend sem. Nam mattis egestas vulputate. Donec posuere ex a leo hendrerit, et ultricies enim fringilla.';
@@ -20,7 +55,6 @@ quoteWordArray.forEach((word) => {
     });
 });
 // Test quote generater end.
-const initialShowAndHide = quoteDisplayElement.querySelectorAll('span.text-word');
 
 quoteInputElement.addEventListener('input', (e) => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span.text-word');
@@ -42,6 +76,8 @@ quoteInputElement.addEventListener('input', (e) => {
     });
     if (!(quoteInputElement.value === ' ')) {
         if (e['data'] === ' ') {
+            solveCount += firstWordSelector.length;
+            wholeCount += 1;
             if (arrayQuote[wordSelectorIndex].querySelectorAll('span.text-word span.incorrect').length == 0 && firstWordSelector.length == arrayQuote[wordSelectorIndex].querySelectorAll('span.text-word span.incorrect').length + arrayQuote[wordSelectorIndex].querySelectorAll('span.text-word span.correct').length) {
                 arrayQuote[wordSelectorIndex].classList.add('correct');
                 quoteInputElement.value = '';
@@ -54,19 +90,24 @@ quoteInputElement.addEventListener('input', (e) => {
                 });
                 quoteInputElement.value = '';
                 wordSelectorIndex++;
+                failureCount += 1;
             }
             lineBreakManager(arrayQuote[wordSelectorIndex], arrayQuote);
             lineHideAndShow(arrayQuote);
+            wpmCalculate();
+            accCalculate();
         }
     } else {
         quoteInputElement.value = '';
     }
 });
 
+const initialShowAndHide = quoteDisplayElement.querySelectorAll('span.text-word');
+
 const lineBreakManager = (domCountElement, domHistory) => {
     const rectPosition = domCountElement.getBoundingClientRect();
     if (!(rectPosition['y'] == 335.75)) {
-        for (let i = 0; i <= wordSelectorIndex; i++) {
+        for (let i = 0; i < wordSelectorIndex; i++) {
             domHistory[i].remove();
         };
         wordSelectorIndex = 0;
@@ -88,81 +129,59 @@ const lineHideAndShow = (domHideElement) => {
 
 window.onload = lineHideAndShow(initialShowAndHide);
 
-//upperSide = Main contents implement
-
-//lowerSide = Protocol
-
-$(document).ready(function () {
-    get_line_info();
-})
-
-// 페이지가 로드 됨과 동시에 영화 대사 정보를 불러옴
-function get_line_info() {
-    $.ajax({
-        type: 'GET',
-        url: '/line',
-        data: {},
-        success: function (response) {
-            console.log(response['movieLines'])
-        }
-    })
-}
-
-// 영화 대사 관련 정보 중 하나의 대사를 랜덤으로 리턴
-// response['movieLines'][random_int()] 리턴 형태:
-//
-//         {
-//             "movie": "Star Wars: Episode V - The Empire Strikes Back",
-//             "num": 0,
-//             "quote": "Do, or do not. There is no try.",
-//             "type": "movie",
-//             "year": 1890
-//         }
-
-function extract_random_line() {
-    $.ajax({
-        type: 'GET',
-        url: '/line',
-        data: {},
-        success: function (response) {
-            {
-                return response['movieLines'][random_int()]['quote']
-            }
-        }
-    })
-}
-
 //0~731 랜덤 정수
 function random_int() {
     const line_no = Math.floor(Math.random() * 732)
     return line_no
 }
 
+//time
+let time = 30;
+let increaseTime = 0;
+
 // 한번만 실행 되는 함수 is_action()
-var is_action = false;
+let is_action = false;
 function timer() {
     if (is_action === true) { return false; }
     is_action = true;
     // 타이머 시간 설정(초 단위)
-    var time = 30;
     // 초 단위로 적어 주면 됩니다.
-    var min = "";
-    var sec = "";
+    let min = "";
+    let sec = "";
 
-    var x = setInterval(function () {
+    let x = setInterval(function () {
         min = parseInt(time / 60);
         sec = time % 60;
 
         if (sec > 9) {sec = time % 60
-        } else {sec = "0" + time %60}
+        } else {sec = "0" + time % 60}
 
         // HTML에 출력 되는 양식
-        document.getElementById("timer").innerHTML = min + ":" + sec;
+        document.getElementById("timer").innerHTML = sec;
         time--;
         // 함수 timer() 가 끝나면 실행 되는 코드
         if (time < 0) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = "Time out";
         }
+        increaseTime += 1/60;
     }, 1000);
+}
+
+
+//WPM counter implement
+
+// wpm 및 cpm 계산
+// wpm 계산식은 변경 가능 있음
+const wpmCalculate = () => {
+    let wpmText = ((solveCount/5)/(increaseTime));
+    //console.log(time);
+    wpmText = wpmText < 0 || ! wpmText || wpmText == Infinity ? 0 : wpmText;
+    wpmTag.innerText = Math.round(wpmText);
+}
+
+const accCalculate = () => {
+    let accuracyText = ((wholeCount - failureCount) / wholeCount);
+    accuracyText = accuracyText < 0 || !accuracyText || accuracyText ==Infinity ? 0:accuracyText;
+    accTag.innerText=Math.round(accuracyText*100) + '%';
 }
